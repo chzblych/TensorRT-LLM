9ae0c8db42244bbc1e538cefd44ec304760b0eef
Yanchao Lu <yanchaol@nvidia.com>
2026-09-16 17:12:10 +0800

    PR: admit requests against token capacity
    Signed-off-by: Yanchao Lu <yanchaol@nvidia.com>

---
+++ admission.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from budget import capacity
+
+def fits(token_count):
+    return token_count <= capacity()
