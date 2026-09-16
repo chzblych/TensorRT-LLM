836638b236d933791e30057835be13d5bc97374a
Yanchao Lu <yanchaol@nvidia.com>
2026-09-16 17:12:10 +0800

    Base: expose a token capacity
    Signed-off-by: Yanchao Lu <yanchaol@nvidia.com>

---
+++ budget.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+def capacity():
+    """Return capacity in tokens."""
+    return 8
